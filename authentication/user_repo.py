7aef48b17933e8db83dfb0a22f39f375d3498e61
apsingh-commits <apsingh@destm.com>
2026-04-07 14:21:01 +0530

    Fix folder structure

--- authentication/user_repo.py
+++
@@ -1,9 +0,0 @@
-class UserRepository:
-    def __init__(self, db):
-        self.collection = db["users"]
-
-    async def get_by_email(self, email: str):
-        return await self.collection.find_one({"email": email})
-
-    async def create_user(self, user_data: dict):
-        return await self.collection.insert_one(user_data)
